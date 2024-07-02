0200f25996674193bf5d0caac484d371f37633ea
AKASH A J <akashjayanthi2019@gmail.com>
2024-07-03 00:57:42 +0530

    user schema created

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-import { log } from 'console';
-import express from 'express';
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port number 3000');
-})
+  console.log("Server is running on port number 3000");
+});
