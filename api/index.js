55d9719da94b86ab8434610a1e3b2420323509e0
AKASH A J <akashjayanthi2019@gmail.com>
2024-07-08 20:04:34 +0530

    creted signup api routes and controllers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,11 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port number 3000");
 });
 
-
-app.get('/test', (req, res) => {
-  res.send('respond from test')
-})
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
