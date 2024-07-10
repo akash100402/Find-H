4afea0a4f4b9f59465f48c1ff1552bc99908e89e
AKASH A J <akashjayanthi2019@gmail.com>
2024-07-10 16:42:30 +0530

    created sign-in api routes and functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,12 @@
-import { Link } from "react-router-dom";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import OAuth from "../components/OAuth";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -11,51 +15,71 @@ export default function SignUp() {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("/api/auth/signup", formData);
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center my-7 font-semibold">Sign up</h1>
-      <form className="flex flex-col gap-4">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
-          placeholder="Enter Username"
+          placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
           onChange={handleChange}
         />
         <input
-          type="text"
-          placeholder="Enter Email"
+          type="email"
+          placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
           onChange={handleChange}
         />
         <input
-          type="text"
-          placeholder="Enter Password"
-          className="border p-3 rounded-lg "
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
           id="password"
           onChange={handleChange}
         />
-        <div className="flex w-full justify-between">
-          <button
-            className="bg-green-500 text-white p-3 px-8 hover:bg-white hover:text-black  rounded-lg uppercase"
-            onSubmit={handleSubmit}
-          >
-            Sign up
-          </button>
-          <button className="bg-violet-600 text-white p-3 rounded-lg uppercase">
-            Continue with google
-          </button>
-        </div>
-        <p>
-          Have an account?
-          <Link to="/sign-in">
-            <span className="text-blue-600 hover:underline"> Sign-in</span>
-          </Link>
-        </p>
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+        {/* <OAuth /> */}
       </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -11,5 +11,6 @@ export default defineConfig({
       },
     },
   },
+
   plugins: [react()],
 });
