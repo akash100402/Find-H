88de00a692a5d5f4851dbadbf75064156cc7e223
AKASH A J <akashjayanthi2019@gmail.com>
2024-07-16 21:36:48 +0530

    protected routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,21 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
-  
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={PrivateRoute}>
+
+          <Route path="/profile" element={<Profile />} />
+          </Route>
       </Routes>
     </BrowserRouter>
   );
